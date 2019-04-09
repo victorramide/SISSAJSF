91c7132240a6ff72ffc94f4ac04261d843f9f309
victorramide <victorramide@victorramide-PC>
2019-04-09 07:10:35 -0300

    Complementando o DAO

--- src/main/java/dao/PedidoDAO.java
+++ src/main/java/dao/PedidoDAO.java
@@ -7,7 +7,10 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import jdbcconnection.SingleConnection;
 import model.PedidoModel;
 
@@ -25,7 +28,7 @@ public class PedidoDAO {
     
     public void create(PedidoModel p) throws SQLException{
         try{
-            String sql = "insert into usuario (numeroProcesso, classe, tipo, prioridade, sentença, oab, data) values (?, ?, ?, ?, ?, ?, ?)";
+            String sql = "insert into usuario (numeroProcesso, classe, tipo, prioridade, sentenca, oab, data) values (?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement insert = connection.prepareStatement(sql);
             insert.setString(1, p.getNumeroProcesso());
             insert.setString(2, p.getClasse());
@@ -42,6 +45,27 @@ public class PedidoDAO {
             connection.close();
         }
     }    
+    
+    public List<PedidoModel> listarPedidoComum() throws SQLException{
+        
+        List<PedidoModel> list = new ArrayList<PedidoModel>();
+        String sql = "select * from pedido where tipo != 'sentenca' and prioridade != true and sentenca != true order by id";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        ResultSet result = statement.executeQuery();
+        
+        while(result.next()){
+            PedidoModel pedidoModel = new PedidoModel();
+            
+            pedidoModel.setNumeroProcesso(result.getString("numeroProcesso"));
+            pedidoModel.setClasse(result.getString("classe"));
+            pedidoModel.setTipo(result.getString("tipo"));
+            pedidoModel.setOab(result.getString("oab"));
+            
+            
+            list.add(pedidoModel);
+        }
+        return list;
+    }
     
     
     
